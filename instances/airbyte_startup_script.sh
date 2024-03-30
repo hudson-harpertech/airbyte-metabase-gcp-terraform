fa3de45178e41c6fc2ca1f3c2c9548bf2741cfc3
hudson-harpertech <102432543+hudson-harpertech@users.noreply.github.com>
2024-03-29 21:59:00 -0700

    Fix Docker repository URL in startup script

--- instances/airbyte_startup_script.sh
+++ instances/airbyte_startup_script.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add --
-sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian buster stable\"
+sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/Linux/debian buster stable'
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -a -G docker $USER
